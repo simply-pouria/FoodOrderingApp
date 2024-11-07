0fb8e4438f12d68005d07dfce284f12a3347c923
pouria :) <pouriapmd@gmail.com>
2024-11-07 17:36:13 +0330

    main page styling

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Colors from '@/src/constants/Colors';
+import products from '@/assets/data/products'
 
+const product = products[0];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Image source={{uri: product.image}} style={styles.image} />
 
-      <Text> Hello World</Text>
+      <Text style={styles.title}> {product.name}</Text>
+      <Text style={styles.price}> {product.price}</Text>
 
     </View>
   );
@@ -13,17 +18,24 @@ export default function TabOneScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
   },
+
+  image:{
+    width: '100%',
+    aspectRatio: 1,
+  },
+
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  price: {
+    color: Colors.light.tint,
+    fontSize: 10,
+    fontWeight: 'bold'
+  }
 });
